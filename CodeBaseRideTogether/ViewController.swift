095e307abda395280c11d78bdb0771919c34df30
kai.jhuang <kevin12342247@gmail.com>
2025-07-23 17:52:37 +0800

    Initial Commit

---
+++ CodeBaseRideTogether/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeBaseRideTogether
+//
+//  Created by Kai Fu Jhuang on 2025/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
